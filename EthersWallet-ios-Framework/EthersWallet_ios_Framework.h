e759a76b2415993a13b177be2e82d2b41a0b6118
Vedran Ozir <vedran.ozir@gmail.com>
2018-01-22 22:08:53 +0100

    add framework target

---
+++ EthersWallet-ios-Framework/EthersWallet_ios_Framework.h
@@ -0,0 +1,22 @@
+//
+//  EthersWallet_ios_Framework.h
+//  EthersWallet-ios-Framework
+//
+//  Created by Vedran Ozir on 21/01/2018.
+//  Copyright © 2018 ethers.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EthersWallet_ios_Framework.
+FOUNDATION_EXPORT double EthersWallet_ios_FrameworkVersionNumber;
+
+//! Project version string for EthersWallet_ios_Framework.
+FOUNDATION_EXPORT const unsigned char EthersWallet_ios_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EthersWallet_ios_Framework/PublicHeader.h>
+
+//#import <EthersWallet/src/ViewControllers/CongifViewControllers/ConfigController.h>
+//#import <ConfigController.h>
+#import "ConfigController.h"
+
